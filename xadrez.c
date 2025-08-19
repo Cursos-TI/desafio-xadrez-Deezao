654b4678ae497730ac14a7f36cdb3dec14e02b50
Deezao <andrebesson19@gmail.com>
2025-08-19 02:21:22 +0000

    começo

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,55 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+   
+    printf("Faça o movimento da sua Torre \n");
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    //Movimento da peca Torre
+    int t = 0;
+    
+    
+    while (t < 5){
+    
+        if (t >= 0)
+    {
+        printf("Torre faz o movimento para direita. \n"); //Direcao do movimento Torre
+    }
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+        t++;
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    }
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    //separacao dos movimentos das pecas
+    printf("\n");
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    //Movimento peca Bispo
+
+    int b;
+
+    do
+    {   
+        printf("Digite a quantidade de casas para movimentar o Bispo: \n");
+        scanf("%d", &b);
+
+        if(b <= 10){
+        printf("o Bispo efetuou %d movimentos para CIMA e DIREITA \n");
+      }      
+    } while (b != 10);
+    
+
+    //Movimento da rainha
+
+    int r, movimento;
+
+    printf("Digite quantos movimentos quer efetuar com a Rainha: \n");
+    scanf("%d", &movimento);
+
+    for (r = 0; r < 8; r++)
+    {
+        printf("Rainha efetuou %d movimentos para esquerda! \n", r + movimento);
+    }
+    
 
     return 0;
 }
